84f801037938361074e1ee373d881db088fb6d71
dhimasdewanto <dhimasbrd@gmail.com>
2020-10-26 09:08:33 +0700

    Add more features in settings repo

--- lib/features/settings/data/repositories/settings_repo_impl.dart
+++ lib/features/settings/data/repositories/settings_repo_impl.dart
@@ -5,8 +5,46 @@ import '../../../../core/failures/failures.dart';
 import '../../domain/repositories/settings_repo.dart';
 
 class SettingsRepoImpl implements SettingsRepo {
-  static const _keyListCharacters = "list_characters";
-  static const _keyListColors = "list_colors";
+  static const _keyListCharacters = "settings_list_characters";
+  static const _keyListColors = "settings_list_colors";
+  static const _keyImageWidth = "settings_image_width";
+  static const _keyFontSize = "settings_font_size";
+  static const _keyLetterSpacing = "settings_letter_spacing";
+  static const _keyRepeatedCharacters = "settings_repeated_characters";
+  static const _keyColorReversed = "settings_color_reversed";
+
+  @override
+  Future<Either<Failures, double>> getFontSize() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final result = prefs.getDouble(_keyFontSize);
+      return right(result);
+    } catch (e) {
+      return left(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Either<Failures, double>> getImageWidth() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final result = prefs.getDouble(_keyImageWidth);
+      return right(result);
+    } catch (e) {
+      return left(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Either<Failures, double>> getLetterSpacing() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final result = prefs.getDouble(_keyLetterSpacing);
+      return right(result);
+    } catch (e) {
+      return left(const Failures.unexpected());
+    }
+  }
 
   @override
   Future<Either<Failures, List<String>>> getListCharacters() async {
@@ -36,15 +74,80 @@ class SettingsRepoImpl implements SettingsRepo {
   }
 
   @override
+  Future<Either<Failures, int>> getRepeatedCharacters() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final result = prefs.getInt(_keyRepeatedCharacters);
+      return right(result);
+    } catch (e) {
+      return left(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Either<Failures, bool>> isColorReversed() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final result = prefs.getBool(_keyColorReversed);
+      return right(result);
+    } catch (e) {
+      return left(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Option<Failures>> setColorReversed({
+    bool isColorReversed = false,
+  }) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setBool(_keyColorReversed, isColorReversed);
+      return none();
+    } catch (e) {
+      return some(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Option<Failures>> setFontSize(double fontSize) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setDouble(_keyFontSize, fontSize);
+      return none();
+    } catch (e) {
+      return some(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Option<Failures>> setImageWidth(double imageWidth) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setDouble(_keyImageWidth, imageWidth);
+      return none();
+    } catch (e) {
+      return some(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Option<Failures>> setLetterSpacing(double letterSpacing) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setDouble(_keyLetterSpacing, letterSpacing);
+      return none();
+    } catch (e) {
+      return some(const Failures.unexpected());
+    }
+  }
+
+  @override
   Future<Option<Failures>> setListCharacters(
     List<String> listCharacters,
   ) async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setStringList(
-        _keyListCharacters,
-        listCharacters,
-      );
+      await prefs.setStringList(_keyListCharacters, listCharacters);
       return none();
     } catch (e) {
       return some(const Failures.unexpected());
@@ -58,10 +161,20 @@ class SettingsRepoImpl implements SettingsRepo {
     try {
       final prefs = await SharedPreferences.getInstance();
       final listStrings = listColors.map((color) => color.toString()).toList();
-      await prefs.setStringList(
-        _keyListColors,
-        listStrings,
-      );
+      await prefs.setStringList(_keyListColors, listStrings);
+      return none();
+    } catch (e) {
+      return some(const Failures.unexpected());
+    }
+  }
+
+  @override
+  Future<Option<Failures>> setRepeatedCharacters(
+    int repeatedCharacters,
+  ) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt(_keyRepeatedCharacters, repeatedCharacters);
       return none();
     } catch (e) {
       return some(const Failures.unexpected());
--- lib/features/settings/domain/repositories/settings_repo.dart
+++ lib/features/settings/domain/repositories/settings_repo.dart
@@ -5,6 +5,19 @@ import '../../../../core/failures/failures.dart';
 abstract class SettingsRepo {
   Future<Either<Failures, List<String>>> getListCharacters();
   Future<Either<Failures, List<int>>> getListColors();
+  Future<Either<Failures, double>> getImageWidth();
+  Future<Either<Failures, double>> getLetterSpacing();
+  Future<Either<Failures, double>> getFontSize();
+  Future<Either<Failures, int>> getRepeatedCharacters();
+  Future<Either<Failures, bool>> isColorReversed();
+  
   Future<Option<Failures>> setListCharacters(List<String> listCharacters);
   Future<Option<Failures>> setListColors(List<int> listColors);
+  Future<Option<Failures>> setImageWidth(double imageWidth);
+  Future<Option<Failures>> setLetterSpacing(double letterSpacing);
+  Future<Option<Failures>> setFontSize(double fontSize);
+  Future<Option<Failures>> setRepeatedCharacters(int repeatedCharacters);
+  Future<Option<Failures>> setColorReversed({
+    bool isColorReversed = false,
+  });
 }
