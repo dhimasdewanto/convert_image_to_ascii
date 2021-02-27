ea110209b32b646594a778b0df48a97e6fdcaf79
dhimasdewanto <dhimasbrd@gmail.com>
2021-02-27 09:03:27 +0700

    Initialize easy localization

--- lib/features/main/presentation/pages/home_page.dart
+++ lib/features/main/presentation/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -96,7 +97,7 @@ class HomePage extends StatelessWidget {
                           size: sizeSmall / 3,
                         ),
                         Text(
-                          "Settings",
+                          tr('settings'),
                           style: textThemeSmall,
                         ),
                       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/dependency_injections/configure_dependencies.dart';
+import 'core/locales.dart';
 import 'core/theme_data.dart';
 import 'features/image_process/presentation/blocs/image_actions/image_actions_bloc.dart';
 import 'features/image_process/presentation/blocs/image_process/image_process_bloc.dart';
@@ -10,7 +12,14 @@ import 'features/settings/presentation/blocs/settings/settings_bloc.dart';
 
 void main() {
   inject();
-  runApp(const MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: mapLocales.values.toList(),
+      path: localePath,
+      fallbackLocale: defaultLocale,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +28,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'ASCII Image',
       themeMode: ThemeMode.dark,
       darkTheme: themeDark,
